5e54cd738ab87913c1b36e203ddec04aaaf69801
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-11-15 11:23:54 +0100

    wip

--- preprocessing.py
+++ preprocessing.py
@@ -21,16 +21,28 @@ class ColumnCleaningStrategy(ABC):
     def fill(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
-class Preprocessing:
+class ColumnClean:
+    def __init__(self, old_column_name: str, new_column_name: str, clean = lambda x: x) -> None:
+        self.old_column_name = old_column_name
+        self.new_column_name = new_column_name
+        self.clean = clean
+    def run(self, old_df, new_df):
+        new_df[self.new_column_name] = old_df[self.old_column_name].apply(lambda x: self.clean(x))
 
-    def __init__(self, strategies: List[ColumnCleaningStrategy]) -> None:
+
+class Preprocessing:
+    def __init__(self, strategies: List[ColumnClean]) -> None:
         self.strategies = strategies
     
-    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        cleaned_df = df.copy()
-        for strategy in self.strategies:
-            cleaned_df = strategy.fill(cleaned_df)
-        return cleaned_df
+    def transform(self, old_df: pd.DataFrame) -> pd.DataFrame:
+        new_df = pd.DataFrame(index=old_df.index)
+        for stragegy in self.strategies:
+            stragegy.run(old_df, new_df)
+        return new_df
+    
+    def get_used_old_columns(self):
+        return [strategy.old_column_name for strategy in self.strategies]
+
         
 
 class CleanNumericAtrributesStrategy(ColumnCleaningStrategy):
