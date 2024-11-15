c39360a637aa2543ce15030c63c00d2ece6ef6cb
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-11-15 17:37:01 +0100

    imporove

--- preprocessing.py
+++ preprocessing.py
@@ -3,24 +3,10 @@ import re
 from typing import List
 import pandas as pd
 
-"""
-preprocessing.py contains every class that handles preprocessing the BindingDB or the merged dataframe with DrugBank
-
-    -> ColumnCleaningStrategy: Used to collect strategies that are used to cleaning the features
-        -> CleanNumericAtrributesStrategy: Checks numeric columns and does the following:
-            -> Replaces NaN values with the given new_class (default: -1)
-            -> The values represented in not only numbers but with characters are cleaned: >21 -> 21
-            -> values are stored in float
-
-    -> Preprocessing: Gets a list of strategies and executes them
 
 """
-class ColumnCleaningStrategy(ABC):
-
-    @abstractmethod
-    def fill(self, df: pd.DataFrame) -> pd.DataFrame:
-        pass
-
+This class represents the data needed to convert a column in the raw dataframe, to a column in the new dataframe.
+"""
 class ColumnClean:
     def __init__(self, old_column_name: str, new_column_name: str, clean = lambda x: x) -> None:
         self.old_column_name = old_column_name
@@ -40,32 +26,8 @@ class Preprocessing:
             stragegy.run(old_df, new_df)
         return new_df.convert_dtypes()
     
-    def get_used_old_columns(self):
+    def get_all_old_columns(self):
         return [strategy.old_column_name for strategy in self.strategies]
 
-        
-
-class CleanNumericAtrributesStrategy(ColumnCleaningStrategy):
-    def keep_just_numeric(self, value, new_class = -1):
-        if type(value) != str:
-            return new_class
-        
-        ## One or more non-digit charachters should be replaced
-        cleaned_val = re.sub(r'[^\d.]+','', str(value)) ## There are random float / str in the dataset?? -> convert to str
-        if(cleaned_val == ''): # It didn't contain any number?
-            return new_class
-        return float(cleaned_val)
-
-    def fill(self, df: pd.DataFrame) -> pd.DataFrame:
-        affinity_cols = ["Ki (nM)", "Kd (nM)"] ## Measures of binding affinity
-        ec_ic = ["EC50 (nM)", "IC50 (nM)"] #### Measures of inhibitory (IC50) and effective concentrations (EC50)
-        bind_unbind = ["kon (M-1-s-1)", "koff (s-1)"] ## Rates of binding / unbinding of ligands
-
-        binding_ligand_efficency_cols = affinity_cols + ec_ic + bind_unbind
-        filtered_df = df.copy()
-        for af_col in binding_ligand_efficency_cols:
-            filtered_df[af_col] = filtered_df[af_col].apply(lambda x: self.keep_just_numeric(x))
-        
-        return filtered_df
-        
-            
+    def get_all_new_columns(self):
+        return [strategy.new_column_name for strategy in self.strategies]
